966c9ff3b61b36a51e92d0f469c2d48217b6d95b
Lahfa Samy <samy@lahfa.xyz>
2022-09-02 17:56:58 +0200

    Debug CI/CD

--- metacall.sh
+++ metacall.sh
@@ -2,20 +2,20 @@
 
 LOC=metacall
 PATH=$PATH:$LOC:$LOC/lib
-PYTHONHOME=$LOC/runtimes/python
-PIP_TARGET=$LOC/runtimes/python/Pip
+#PYTHONHOME=$LOC/runtimes/python
+#PIP_TARGET=$LOC/runtimes/python/Pip
 # Python
-PATH=$PATH:$LOC/runtimes/python:$LOC/runtimes/python/Scripts
-# NodeJS
-PATH=$PATH:$LOC/lib/runtimes/nodejs:$LOC/lib/runtimes/nodejs/lib
-# DotNet Core
-PATH=$PATH:$LOC/lib/runtimes/dotnet:$LOC/lib/runtimes/dotnet/host/fxr/ # TODO: Add version
+#PATH=$PATH:$LOC/runtimes/python:$LOC/runtimes/python/Scripts
+## NodeJS
+#PATH=$PATH:$LOC/lib/runtimes/nodejs:$LOC/lib/runtimes/nodejs/lib
+## DotNet Core
+#PATH=$PATH:$LOC/lib/runtimes/dotnet:$LOC/lib/runtimes/dotnet/host/fxr/ # TODO: Add version
 
 # Check if it is running a package manager and execute it
 # WTF
 
 # MetaCall Environment
-CORE_ROOT=$LOC/runtimes/dotnet/shared/Microsoft.NETCore.App/ # TODO: Add version
+#CORE_ROOT=$LOC/runtimes/dotnet/shared/Microsoft.NETCore.App/ # TODO: Add version
 LOADER_LIBRARY="$LOC/lib"
 SERIAL_LIBRARY_PATH="$LOC/lib"
 DETOUR_LIBRARY_PATH="$LOC/lib"
@@ -24,4 +24,4 @@ PORT_LIBRARY_PATH="$LOC/lib"
 CONFIGURATION_PATH=$LOC/configurations/global.json
 
 # Execute MetaCall CLI 
-$LOC/metacallcli.app/Contents/MacOS/metacallcli $@
+$LOC/metacallcli $@
